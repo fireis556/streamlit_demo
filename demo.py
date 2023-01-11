5e18e88ba8a9b704ba6bd36dc7dc26949adaa85a
jonathan.chang <jonathan.chang@egistec.com>
2023-01-11 14:39:33 +0800

    V6

--- demo.py
+++ demo.py
@@ -112,19 +112,18 @@ def init_df(total, start_val=0,end_val=9999, start_direction='Left'):
             ret_df.setdefault(f'U{usr_idx}', []).append(check_is_need_to_pass(num))
     ret_df = dict(sorted(ret_df.items(), key=lambda item:int(item[0].replace('U',''))))
     return pd.DataFrame(ret_df)
-def update_df(usr_list, start_val=1,end_val=9999, start_direction='Left'):
+def update_df(usr_list,tar_num=7, start_val=1,end_val=9999, start_direction='Left'):
     ret_df = dict()
-    print(usr_list)
     start_direction = start_direction.lower()
     end_val = round_to_multiple(len(usr_list),start_val,end_val+1)
     if start_direction == 'left':
         for idx, num in enumerate(range(start_val, end_val)):
             usr_idx = (idx) % len(usr_list)
-            ret_df.setdefault(usr_list[usr_idx], []).append(check_is_need_to_pass(num))
+            ret_df.setdefault(usr_list[usr_idx], []).append(check_is_need_to_pass(num, tar_num))
     else:
         for idx,num in enumerate(range(start_val, end_val)):
             usr_idx = len(usr_list) - (idx % len(usr_list))
-            ret_df.setdefault(usr_list[usr_idx-1], []).append(check_is_need_to_pass(num))
+            ret_df.setdefault(usr_list[usr_idx-1], []).append(check_is_need_to_pass(num, tar_num))
     ret_df = dict(sorted(ret_df.items(), key=lambda item:int(item[0].replace('U',''))))
     return pd.DataFrame(ret_df)
 
@@ -143,25 +142,39 @@ if total:
     st.write('<style>div.row-widget.stRadio > div{flex-direction:row;justify-content: center;} </style>', unsafe_allow_html=True)
     st.write('<style>div.st-bf{flex-direction:column;} div.st-ag{font-weight:bold;padding-left:2px;}</style>', unsafe_allow_html=True)
     choose = st.radio('落在第幾個:',range(1,total+1))
-    with st.sidebar:
-        MAX_VAL = int(st.number_input('上限',value=500,max_value=9999)) 
+    sub_1,sub_2,sub_3 = st.columns(3)
+    if 'count' not in st.session_state:
+        st.session_state.count = 0
+    with sub_1:
+        MAX_VAL = int(st.number_input('上限',value=500,max_value=9999))
+    with sub_2: 
         MIN_VAL = int(st.number_input('下限',value=1, min_value=0))
+    with sub_3:
+        TAR_NUM = int(st.slider('逢N過',value=7, min_value=2,max_value=9))
     ori_usr_list=  [f'U{x}' for x in range(1, total+1)]
     # st.multiselect("hello", df_left.columns, default=list(df_left.columns))
-    remaining_usr_list = st.multiselect("hello", ori_usr_list, default=ori_usr_list)
-    df_left = update_df(remaining_usr_list,MIN_VAL,MAX_VAL).style.applymap(highlight_col, subset=pd.IndexSlice[:, [f'U{choose}']])
-    col1, col2 = st.columns(2)
-    with col1: # 從左到右
-        df_left = update_df(remaining_usr_list,MIN_VAL,MAX_VAL).style.applymap(highlight_col, subset=pd.IndexSlice[:, [f'U{choose}']])
-        st.markdown(hide_dataframe_row_index, unsafe_allow_html=True)
-        st.dataframe(df_left)
-    with col2:
-        df_right = update_df(remaining_usr_list,MIN_VAL,MAX_VAL,'right').style.applymap(highlight_col, subset=pd.IndexSlice[:, [f'U{choose}']])
-        st.markdown(hide_dataframe_row_index, unsafe_allow_html=True)
-        st.dataframe(df_right)
-    num = st.number_input('text', format='%d')
-    st.text(num)
-    if num:
-        check = st.button('GOGO')
-        if check:
-            st.text(check_is_need_to_pass(num))
+    remaining_usr_list = st.multiselect("生存名單", ori_usr_list, default=ori_usr_list)
+    direction = st.radio('從哪個方向開始',['left', 'right'])
+    sub_4,sub_5 = st.columns(2)
+    with sub_4:
+        checked = st.button('+1')
+        if checked:
+            st.session_state.count += 1
+    with sub_5:
+        reset = st.button('Reset')
+        if reset:
+            st.session_state.count = 0
+    ori_df = pd.DataFrame(update_df(remaining_usr_list,TAR_NUM,MIN_VAL,MAX_VAL, direction).iloc[[st.session_state.count]]).style.applymap(highlight_col, subset=pd.IndexSlice[:, [f'U{choose}']])
+    # df = ori_df.iloc[[st.session_state.count]]
+    st.markdown(hide_dataframe_row_index, unsafe_allow_html=True)
+    st.dataframe(ori_df)
+
+    # col1, col2 = st.columns(2)
+    # with col1: # 從左到右
+    #     df_left = update_df(remaining_usr_list,TAR_NUM,MIN_VAL,MAX_VAL).style.applymap(highlight_col, subset=pd.IndexSlice[:, [f'U{choose}']])
+    #     st.markdown(hide_dataframe_row_index, unsafe_allow_html=True)
+    #     st.dataframe(df_left)
+    # with col2:
+    #     df_right = update_df(remaining_usr_list,TAR_NUM,MIN_VAL,MAX_VAL,'right').style.applymap(highlight_col, subset=pd.IndexSlice[:, [f'U{choose}']])
+    #     st.markdown(hide_dataframe_row_index, unsafe_allow_html=True)
+    #     st.dataframe(df_right)
