e38f24801c36e35331a22e0b0eef7ba8bd06191c
jonathan.chang <jonathan.chang@egistec.com>
2023-01-11 16:15:17 +0800

    V7

--- demo.py
+++ demo.py
@@ -11,83 +11,7 @@ def check_is_need_to_pass(num, tar_num=TAR):
         return "PASS!"
     else:
         return str(num)
-def filter_dataframe(df: pd.DataFrame) -> pd.DataFrame:
-    """
-    Adds a UI on top of a dataframe to let viewers filter columns
 
-    Args:
-        df (pd.DataFrame): Original dataframe
-
-    Returns:
-        pd.DataFrame: Filtered dataframe
-    """
-    modify = st.checkbox("Add filters")
-
-    if not modify:
-        return df
-
-    df = df.copy()
-
-    # Try to convert datetimes into a standard format (datetime, no timezone)
-    # for col in df.columns:
-    #     if is_object_dtype(df[col]):
-    #         try:
-    #             df[col] = pd.to_datetime(df[col])
-    #         except Exception:
-    #             pass
-
-    #     if is_datetime64_any_dtype(df[col]):
-    #         df[col] = df[col].dt.tz_localize(None)
-
-    modification_container = st.container()
-
-    with modification_container:
-        to_filter_columns = st.multiselect("Filter dataframe on", df.columns)
-        for column in to_filter_columns:
-            left, right = st.columns((1, 20))
-            # Treat columns with < 10 unique values as categorical
-            if is_categorical_dtype(df[column]) or df[column].nunique() < 10:
-                user_cat_input = right.multiselect(
-                    f"Values for {column}",
-                    df[column].unique(),
-                    default=list(df[column].unique()),
-                )
-                df = df[df[column].isin(user_cat_input)]
-            elif is_numeric_dtype(df[column]):
-                _min = float(df[column].min())
-                _max = float(df[column].max())
-                step = (_max - _min) / 100
-                user_num_input = right.slider(
-                    f"Values for {column}",
-                    min_value=_min,
-                    max_value=_max,
-                    value=(_min, _max),
-                    step=step,
-                )
-                df = df[df[column].between(*user_num_input)]
-            elif is_datetime64_any_dtype(df[column]):
-                user_date_input = right.date_input(
-                    f"Values for {column}",
-                    value=(
-                        df[column].min(),
-                        df[column].max(),
-                    ),
-                )
-                if len(user_date_input) == 2:
-                    user_date_input = tuple(map(pd.to_datetime, user_date_input))
-                    start_date, end_date = user_date_input
-                    df = df.loc[df[column].between(start_date, end_date)]
-            else:
-                user_text_input = right.text_input(
-                    f"Substring or regex in {column}",
-                )
-                if user_text_input:
-                    df = df[df[column].astype(str).str.contains(user_text_input)]
-
-    return df
-
-# with st.sidebar:
-#     st.radio("當前人數:", range(1,28))
 def button_clicked():
     print('5566')
 
@@ -129,7 +53,7 @@ def update_df(usr_list,tar_num=7, start_val=1,end_val=9999, start_direction='Lef
 
 def highlight_col(row):
     return 'background-color: yellow'
-st.set_page_config(layout="wide")
+# st.set_page_config(layout="wide")
 hide_dataframe_row_index = """
             <style>
             .row_heading.level0 {display:none}
@@ -164,9 +88,11 @@ if total:
         reset = st.button('Reset')
         if reset:
             st.session_state.count = 0
-    ori_df = pd.DataFrame(update_df(remaining_usr_list,TAR_NUM,MIN_VAL,MAX_VAL, direction).iloc[[st.session_state.count]]).style.applymap(highlight_col, subset=pd.IndexSlice[:, [f'U{choose}']])
+    tmp_df = update_df(remaining_usr_list,TAR_NUM,MIN_VAL,MAX_VAL, direction)
+    start_idx = 0 if st.session_state.count - 1 < 0 else st.session_state.count - 1
+    end_idx = tmp_df.shape[0] if st.session_state.count + 1 > tmp_df.shape[0] else st.session_state.count + 1
+    ori_df = pd.DataFrame(tmp_df.iloc[[start_idx,st.session_state.count,end_idx]]).reset_index(drop=True).style.applymap(highlight_col, subset=pd.IndexSlice[1, [f'U{choose}']])
     # df = ori_df.iloc[[st.session_state.count]]
-    st.markdown(hide_dataframe_row_index, unsafe_allow_html=True)
     st.dataframe(ori_df)
 
     # col1, col2 = st.columns(2)
